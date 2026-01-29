d2ec9a7d264adae88b10232001917e43e4aba3e3
ValkyRiE2117 <aritzagk@gmail.com>
2026-01-29 14:56:12 +0700

    first commit to adding user struct

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"backend-golang/config"
+	"backend-golang/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	//load config .env
+	config.LoadEnv()
+
+	//inisialisasi database
+	database.InitDB()
+
+	//inisialiasai Gin
+	router := gin.Default()
+
+	//membuat route dengan method GET
+	router.GET("/", func(c *gin.Context) {
+
+		//return response JSON
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	//mulai server
+	router.Run(":" + config.GetEnv("APP_PORT", "3000"))
+}
